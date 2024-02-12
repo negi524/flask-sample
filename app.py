c5d13196a40af653bd292af836aac7031cb2dd33
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-02-12 14:35:40 +0900

    feat: ログ出力処理実装

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import logging
+import logging.config
+import yaml
 
 app = Flask(__name__)
 
 response = {"userId": 1, "userName": "hoge"}
+
+# ログ設定読み込み
+with open("./logsetting.yml", "r") as file:
+    logging.config.dictConfig(yaml.safe_load(file))
 
 
 @app.route("/")
